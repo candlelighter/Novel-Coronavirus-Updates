0e81422836b4c777f15c7f8d401a0aa2c2a89779
candlelighter <xintian.lin@gmail.com>
2020-02-01 10:56:09 -0800

    Add files via upload

--- plot.py
+++ plot.py
@@ -7,13 +7,14 @@ import matplotlib
 matplotlib.rc('xtick', labelsize=20)     
 matplotlib.rc('ytick', labelsize=20)
 
-df=pd.read_csv('Updates_NC.csv',encoding='GB2312') # 'ISO-8859-1') # 'utf-8') #'cp1252'
+#df=pd.read_csv('Updates_NC.csv',encoding='GB2312') # 1/31, encoding changed from utf-8 to gb2312.
+df=pd.read_csv('Updates_NC.csv',encoding='utf-8') # 2/1, encoding changed back to utf-8
 # assuming the date format of "month月day日'
 columns={'year','month','day'}
 df1=pd.DataFrame(index=df.index, columns=columns)
 #df1['month']=pd.to_numeric(df['确诊/出院'].str.replace('月[0-9].','').str.replace('日',''))
 #df1['day']=pd.to_numeric(df['确诊/出院'].str.replace('[0-9]*月','').str.replace('日',''))
-#报道时间
+# 1/31 new data format 
 df1['month']=pd.to_numeric(df['报道时间'].str.replace('月[0-9].','').str.replace('日',''))
 df1['day']=pd.to_numeric(df['报道时间'].str.replace('[0-9]*月','').str.replace('日',''))
 df1['year']=df1['month'] # initiate
@@ -36,6 +37,8 @@ for k in  index:
     df2['death'][k]=df3['新增死亡'].sum() # add up the death
 print(df2)
 
+# overall data average slope
+
 # plot
 fig, ax1 = plt.subplots(figsize=(12,8))
 ax1.semilogy(df2['dt'],df2['infection'],'bo-', df2['dt'],df2['death'],'co-', linewidth=3)
@@ -54,11 +57,24 @@ def growthRate(df, list, N_ave):
                 stats=linregress(x[k-N_ave+1:k+1],y[k-N_ave+1:k+1])
                 df[item+'_gr'].loc[k]=stats.slope
     return df
+
+def aveGrowthRate(df, list): # overall slope
+    result=[]
+    for item in list:
+        x=pd.to_numeric(df['dt'])/86400e9
+        y=np.log(pd.to_numeric(df[item]))
+        stats=linregress(x.iloc[[0, -1]], y.iloc[[0, -1]])
+        result.append(stats.slope)
+    return result
+
 N_ave=5 # number of days to average from past data points (including the current data point)    
 growthRate(df2, ['infection', 'death'], N_ave)
 print(df2)
+print("The Average Growth rate for"+" infection and death with all data points"+ " are:")
+print(aveGrowthRate(df2, ['infection', 'death']))  # print the over all growth rate with all the available data
+
 ax2=ax1.twinx()
-ax2.plot(df2['dt'], df2['infection_gr'], 'b-.', df2['dt'], df2['death_gr'], 'c-.', linewidth=2)
+ax2.plot(df2['dt'], df2['infection_gr'], 'bo-.', df2['dt'], df2['death_gr'], 'co-.', linewidth=2)
 ax2.legend(['infection growth rate', 'death growth rate'], loc='lower right', fontsize=20)
 #ax1.grid(b=True, which='major')
 #ax2.grid(b=True, which='both', axis='y', color='k', linestyle='-.', linewidth=1)
